7440607659cca65f9cd925dbe7036b9b46be2bb8
abdulaziz27 <doelziez212@gmail.com>
2025-01-14 15:20:41 +0700

    fix: order date format

--- app/Http/Controllers/API/OrderItemController.php
+++ app/Http/Controllers/API/OrderItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\OrderItem;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -19,7 +20,10 @@ class OrderItemController extends Controller
         $query->select('order_items.*', DB::raw('(SELECT name FROM products WHERE products.id = order_items.product_id) AS product_name'));
 
         if ($start_date && $end_date) {
-            $query->whereBetween('order_items.created_at', [$start_date, $end_date]);
+            $start = Carbon::parse($start_date)->startOfDay();
+            $end = Carbon::parse($end_date)->endOfDay();
+
+            $query->whereBetween('order_items.created_at', [$start, $end]);
         }
 
         $orderItems = $query->get();
@@ -30,18 +34,21 @@ class OrderItemController extends Controller
         ], 200);
     }
     public function orderSales(Request $request)
-{
-    $startDate = $request->input('start_date');
-    $endDate = $request->input('end_date');
-    $query = OrderItem::select('order_items.product_id', DB::raw('(SELECT name FROM products WHERE products.id = order_items.product_id) AS product_name'), DB::raw('SUM(order_items.quantity) as total_quantity'))
-        ->groupBy('order_items.product_id');
-    if ($startDate && $endDate) {
-        $query->whereBetween(DB::raw('DATE(order_items.created_at)'), [$startDate, $endDate]);
+    {
+        $startDate = $request->input('start_date');
+        $endDate = $request->input('end_date');
+        $query = OrderItem::select('order_items.product_id', DB::raw('(SELECT name FROM products WHERE products.id = order_items.product_id) AS product_name'), DB::raw('SUM(order_items.quantity) as total_quantity'))
+            ->groupBy('order_items.product_id');
+        if ($startDate && $endDate) {
+            $start = Carbon::parse($startDate)->startOfDay();
+            $end = Carbon::parse($endDate)->endOfDay();
+
+            $query->whereBetween('order_items.created_at', [$start, $end]);
+        }
+        $totalProductSold = $query->orderBy('total_quantity', 'desc')->get();
+        return response()->json([
+            'status' => 'success',
+            'data' => $totalProductSold
+        ], 200);
     }
-    $totalProductSold = $query->orderBy('total_quantity', 'desc')->get();
-    return response()->json([
-        'status' => 'success',
-        'data' => $totalProductSold
-    ], 200);
-}
 }
--- resources/views/components/error-footer.blade.php
+++ resources/views/components/error-footer.blade.php
@@ -1,3 +1,3 @@
 <div class="simple-footer mt-5">
-    Copyright &copy; JAGOFLUTTER 2024
+    Copyright &copy; Seblak Sulthane 2024
 </div>
